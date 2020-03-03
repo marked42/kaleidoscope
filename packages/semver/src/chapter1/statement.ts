c47df317fc6212e730066c6d46aacaeef320021f
penghui <pzhang2@xiaohongshu.com>
2020-03-03 22:34:56 +0800

    docs: refactoring

--- packages/semver/src/chapter1/statement.ts
+++ packages/semver/src/chapter1/statement.ts
@@ -58,41 +58,51 @@ function statement(invoice: Invoice, plays: Plays): string {
   let result = `Statement for ${invoice.customer}\n`
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
+  // remove local variable
+  function playFor(perf: IPerformance): Play {
+    return plays[perf.playID]
+  }
+
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
-    let thisAmount = 0
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-
-      case "comedy":
-        thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
-        }
-        thisAmount += 300 * perf.audience
-        break
-
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
+    let thisAmount = amountFor(perf, playFor(perf))
 
     volumeCredits += Math.max(perf.audience - 30, 0)
-    if ("comedy" === play.type) {
+    if ("comedy" === playFor(perf).type) {
       volumeCredits += Math.floor(perf.audience)
     }
 
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}) seats\n`
+    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}) seats\n`
     totalAmount += thisAmount
   }
 
   result += `Amount owned is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
+
+  return result
+}
+
+function amountFor(perf: IPerformance, play: Play): number {
+  let result = 0
+
+  switch (play.type) {
+    case "tragedy":
+      result = 40000
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30)
+      }
+      break
+
+      case "comedy":
+        result = 30000
+        if (perf.audience > 20) {
+          result += 10000 + 500 * (perf.audience - 20)
+        }
+        result += 300 * perf.audience
+        break
+
+      default:
+        throw new Error(`unknown type: ${play.type}`)
+  }
 
   return result
 }
