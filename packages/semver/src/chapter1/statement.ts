68f60f2e15e695736a08c957fea6e058e0ad9797
penghui <penghui.zhang@foxmail.com>
2020-03-04 15:24:38 +0800

    docs: refactor v3

--- packages/semver/src/chapter1/statement.ts
+++ packages/semver/src/chapter1/statement.ts
@@ -56,55 +56,67 @@ function statement(invoice: Invoice, plays: Plays): string {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
+  // const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
-  // remove local variable
-  function playFor(perf: IPerformance): Play {
-    return plays[perf.playID]
+  function format(num: number): string {
+    const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
+
+    return format(num)
   }
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
-
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience)
-    }
-
-    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}) seats\n`
-    totalAmount += thisAmount
+    // inline variable
+    // let thisAmount = amountFor(perf)
+    volumeCredits += volumeCreditsFor(perf)
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}) seats\n`
+    totalAmount += amountFor(perf)
   }
 
   result += `Amount owned is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
 
   return result
-}
 
-function amountFor(perf: IPerformance, play: Play): number {
-  let result = 0
-
-  switch (play.type) {
-    case "tragedy":
-      result = 40000
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30)
-      }
-      break
-
-      case "comedy":
-        result = 30000
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20)
-        }
-        result += 300 * perf.audience
-        break
-
-      default:
-        throw new Error(`unknown type: ${play.type}`)
+  // remove local variable
+  function playFor(perf: IPerformance): Play {
+    return plays[perf.playID]
   }
 
-  return result
+  function amountFor(perf: IPerformance): number {
+    let result = 0
+
+    switch (playFor(perf).type) {
+      case "tragedy":
+        result = 40000
+        if (perf.audience > 30) {
+          result += 1000 * (perf.audience - 30)
+        }
+        break
+
+        case "comedy":
+          result = 30000
+          if (perf.audience > 20) {
+            result += 10000 + 500 * (perf.audience - 20)
+          }
+          result += 300 * perf.audience
+          break
+
+        default:
+          throw new Error(`unknown type: ${playFor(perf).type}`)
+    }
+
+    return result
+  }
+
+  function volumeCreditsFor(perf: IPerformance): number {
+    let result = Math.max(perf.audience - 30, 0)
+    if ("comedy" === playFor(perf).type) {
+      result += Math.floor(perf.audience / 10)
+    }
+
+    return result
+  }
 }
+
 
 console.log(statement(invoices[0], plays))
